cc5e3fd08bdc58ca5d4ce4b44200b39d758ff0f7
sinnenicht <64986225+sinnenicht@users.noreply.github.com>
2020-05-26 18:36:55 -0400

    Update Converter.java
    Removed print statements that had been for debugging purposes.

--- src/converter/Converter.java
+++ src/converter/Converter.java
@@ -87,7 +87,6 @@ public class Converter {
 
     private long convertFractionalNumToDecimal(FractionalPart fractionalPart, int sourceRadix, Maps maps) {
         Map<Character, Integer> charIntMap = maps.getCharIntMap();
-        System.out.println("Converting fractional number from base " + sourceRadix + " to decimal");
 
         String fractional = fractionalPart.getNumberString();
         int numDigits = fractional.length();
@@ -96,9 +95,6 @@ public class Converter {
 
         for (int index = 0; index < numDigits; index++) {
             double numerator = charIntMap.get(fractional.charAt(index));
-            System.out.println("total = " + total + " + " + numerator + " / " + base + " = " + (total + numerator / base));
-            System.out.println("new base = " + base + "^2 = " + base * base);
-            System.out.println();
             total += numerator / base;
             base *= sourceRadix;
         }
@@ -107,38 +103,29 @@ public class Converter {
         decimal.append(total);
         decimal.delete(0, 2);
         String decimalFraction = decimal.toString();
-        System.out.println("decimal fraction: " + decimalFraction);
-        System.out.println();
 
         return Long.parseLong(decimalFraction);
     }
 
     private String convertFractionalDecimalToRadix(long decimalNum, int targetRadix, Maps maps) {
         Map<Integer, Character> intCharMap = maps.getIntCharMap();
-        System.out.println("Converting decimal number to base " + targetRadix);
 
         // turn decimalNum into a double
         StringBuilder decimal = new StringBuilder();
         decimal.append(decimalNum);
         decimal.insert(0, "0.");
         double decimalFraction = Double.parseDouble(String.valueOf(decimal));
-        System.out.println("decimal fraction as double: " + decimalFraction);
-        System.out.println();
 
         // convert double to desired radix
         StringBuilder fractional = new StringBuilder();
 
         for (int digits = 0; digits < 5; digits++) {
-            System.out.println("result = " + decimalFraction + " * " + targetRadix + " = " + decimalFraction * targetRadix);
             decimalFraction *= targetRadix;
             String[] splitNum = String.valueOf(decimalFraction).split("\\.");
             fractional.append(intCharMap.get(Integer.parseInt(splitNum[0])));
-            System.out.println("numerical value: " + splitNum[0] + " -> " + intCharMap.get(Integer.parseInt(splitNum[0])));
 
             String newNumToMultiply = "0." + splitNum[1];
             decimalFraction = Double.parseDouble(newNumToMultiply);
-            System.out.println("new num to multiply: " + decimalFraction);
-            System.out.println();
         }
 
         return String.valueOf(fractional);
